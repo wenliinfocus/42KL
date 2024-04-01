ac1f8d609f2477ac5fc3184e294b9a12174d11c0
Wen Li <152971905+wenliinfocus@users.noreply.github.com>
2024-04-01 12:01:40 +0800

    Update and rename C02_ex01 to C02/ex01_ft_strncpy.c

--- C02/ex01_ft_strncpy.c
+++ C02/ex01_ft_strncpy.c
@@ -1,12 +1,12 @@
 Files to turn in: ft_strncpy.c
 
-  /*  The strncpy() function copies at most n characters from s2 into s1.  If
-     s2 is less than n characters long, the remainder of s1 is filled with
-     '\0' characters.  Otherwise, s1 is not terminated.
+/*  
+The strncpy() function copies at most n characters from s2 into s1. 
+If s2 is less than n characters long, the remainder of s1 is filled with '\0' characters.  Otherwise, s1 is not terminated.
   RETURN VALUES
      The strcpy() and strncpy() functions return s1.
-  */
-/* The C library function char *strncpy(char *dest, const char *src, size_t n) copies up to the n characters from the string pointed to, by src to dest.
+
+The C library function char *strncpy(char *dest, const char *src, size_t n) copies up to the n characters from the string pointed to, by src to dest.
 In the case whereby the length of src is less than that of n, the remainder of dest will be padded with null bytes.
 */
 
