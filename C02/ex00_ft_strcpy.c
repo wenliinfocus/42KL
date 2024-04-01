687797fc4f77785280523c7b02a29dede21d92f4
Wen Li <152971905+wenliinfocus@users.noreply.github.com>
2024-04-01 10:34:29 +0800

    Update and rename C02_ex00 to C02/ex00_ft_strcpy.c

--- C02/ex00_ft_strcpy.c
+++ C02/ex00_ft_strcpy.c
@@ -1,10 +1,10 @@
 Files to turn in : ft_strcpy.c
+	
 /* The strncpy() function copies at most n characters from s2 into s1. (including
      the terminating '\0' character).
 RETURN VALUES
      The strcpy() and strncpy() functions return s1
-*/
-/*
+
 src (source string): This is the pointer to the sring to be copied. const char* type of source string ensures that the function will not change the content of the source string.
 dest (destination string): This is the pointer to the array where the other string will be copied.
 */
