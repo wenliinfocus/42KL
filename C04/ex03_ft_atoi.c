7a07aaa56a43952cab49b57c8bbd61080d0f2ee6
Wen Li <152971905+wenliinfocus@users.noreply.github.com>
2024-04-02 18:46:47 +0800

    Update ex03_ft_atoi.c

--- C04/ex03_ft_atoi.c
+++ C04/ex03_ft_atoi.c
@@ -20,17 +20,17 @@ int  ft_atoi(char *str)
  
   result = 0;
   sign = 1;
-  while (*str == 32 || (*str >= 9 && *str <= 13))   // Skip any leading whitespace characters (space, tab, newline, etc.)
+  while (str == 32 || (str >= 9 && str <= 13))   // Skip any leading whitespace characters (' ' space, '\t' tab, '\n' newline, '\v' vertical space, 't' horizontal space, '\f' new page, '\r' carriage return', etc.)
     str++;
-  while (*str == '-' || *str == '+')  // Handle the sign of the integer (+/-)
+  while (str == '-' || str == '+')  // Handle the sign of the integer (+/-)
   {
-    if (*str == '-')
+    if (str == '-')
       sign *= -1;  // Toggle the sign if a '-' character is encountered
     str++;
   }
-  while (*str >= '0' && *str <= '9')  // Convert the string representation of the integer to an actual integer value
+  while (str >= '0' && str <= '9')  // Convert the string representation of the integer to an actual integer value
   {
-    result = result * 10 + *str - '0';  // Multiply the current result by 10 and add the numerical value of the current character
+    result = result * 10 + str - '0';  // Multiply the current result by 10 and add the numerical value of the current character
     str++;
   }
   return (result * sign);  // Apply the sign to the final result and return it
