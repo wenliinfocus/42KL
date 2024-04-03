0048da6ce3a13c2bc55f362bc8503e0b3ee82d7d
Wen Li <152971905+wenliinfocus@users.noreply.github.com>
2024-04-03 11:55:10 +0800

    Update ex00_ft_strdup.c

--- C07/ex00_ft_strdup.c
+++ C07/ex00_ft_strdup.c
@@ -6,6 +6,8 @@ Reproduce the behavior of the function strdup (man strdup).
 - The strdup() function allocates sufficient memory for a copy of the string s1, does the copy, and returns a pointer to it.
 - The pointer may subsequently be used as an argument to the function free(3).
 - If insufficient memory is available, NULL is returned and errno is set to ENOMEM.
+
+The <stdlib.h> header file contains general-purpose functions, macros, and types, such as malloc() , free() , and EXIT_SUCCESS .
 */
 
 mkdir ex00
@@ -13,42 +15,29 @@ cd ex00
 man strup
 vim ft_strdup.c
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
-
-unsigned int  get_len(char *str)
-{
-  unsigned int  i;
-
-  i = 0;
-  while (str[i] != '\0')
-    i++;
-  return (i);
-}
-
-char  *str_cpy(char *str, char *dest)
-{
-  unsigned int  i;
-
-  i = 0;
-  while (str[i] != '\0')
-  {
-    dest[i] = str[i];
-    i++;
-  }
-  dest[i] = '\0';
-  return (dest);
-}
+  
+#include <stdlib.h> 
 
 char  *ft_strdup(char *src)
 {
-  unsigned int  i;
   char *dest;
+  int  i;
 
-  i = get_len(src);
-  dest = (char *)malloc(sizeof(char) * i);
-  if (!(dest))
-    return (NULL)
-  return (str_cpy(src, dest));
+  i = 0;
+  while (src[i])
+    i++;
+  dest = malloc(sizeof(src) * i);
+  i = 0;
+  if (dest == NULL)
+    return (0)
+  else
+  {
+    while (src[i])
+    {
+      dest[i] = src[i];
+      i++;
+    }
+    dest[i] = '\0';
+    return (dest);
+  }
 }
