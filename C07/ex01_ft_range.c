8aa023cc2d290c79cba163ef6ef6164da4757e0a
Wen Li <152971905+wenliinfocus@users.noreply.github.com>
2024-04-03 11:50:31 +0800

    Create ex01_ft_range.c

---
+++ C07/ex01_ft_range.c
@@ -0,0 +1,27 @@
+Files to turn in: ft_range.c
+
+/*
+Create a function ft_range which returns an array of ints. This int array should contain all values between min and max.
+Min included – max excluded.
+*/
+
+#include <stdlib.h>
+
+int  *ft_range(int min, int max)
+{
+  int  *tab;
+  int  i;
+
+  if (min >= max)
+    return (NULL);
+  tab = (int*)malloc(sizeof(*tab) * (max - min));
+  if (tab == NULL)
+    return (NULL);
+  else
+  {
+    i = -1;
+    while (++i < max - min)
+      tab[i] = min + i;
+    return (tab);
+  }
+}
